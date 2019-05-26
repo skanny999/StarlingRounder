a146c57e0d104e366815680e35dce292d6c35c13
skanny999 <skanny999@me.com>
2019-05-26 15:48:18 +0100

    Create UI

---
+++ StarlingRounder/AccountViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  AccountViewModel.swift
+//  StarlingRounder
+//
+//  Created by Riccardo on 26/05/2019.
+//  Copyright © 2019 RiccardoScanavacca. All rights reserved.
+//
+
+import Foundation
+
+class AccountViewModel: NSObject {
+    
+    
+    override init() {
+        super.init()
+        
+    }
+    
+    
+}
